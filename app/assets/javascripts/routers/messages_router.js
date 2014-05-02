20db44ba49fde668eb54d5d6a3f4191c5fc0d732
Gavin Castro <gav.castro@gmail.com>
2014-05-02 10:33:24 -0400

    Adds GeoLocation to Posts

--- app/assets/javascripts/routers/messages_router.js
+++ app/assets/javascripts/routers/messages_router.js
@@ -15,16 +15,14 @@ window.FBClone.Routers.MessagesRouter = Backbone.Router.extend({
 		var threadNewView = new FBClone.Views.ThreadNew
     	this.$rootEl.html(threadNewView.render().$el)
 
-
 		var threadIndex = new FBClone.Views.ThreadIndex({ collection: this.messageThreads })	
 		this.$rootEl.append(threadIndex.render().$el)
 
+		//NEED TO IMPLEMENT A SWAPVIEWS TO REMOVE THESE
 	},
 
 	threadShow: function(id) {
-
 		var thread = FBClone.messageThreads.get(id);
-
 		var threadShowView = new FBClone.Views.ThreadShow({ model: thread })
 
 		this.$rootEl.html(threadShowView.render().$el)
